848bef670df88ee49053a93788acd2e35395037a
Sebastian-Leal <sebastian.leal@mercadolibre.com.co>
2021-10-19 19:50:31 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:world_timer/pages/choose_location.dart';
+
+import 'package:world_timer/pages/home.dart';
+import 'package:world_timer/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(initialRoute: '/', routes: <String, WidgetBuilder>{
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  }));
+}
